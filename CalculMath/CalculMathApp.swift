edeb0989f2ca2c3f676cca24d85a97f71b992690
tonyolyva <olyvatony@gmail.com>
2025-03-13 19:02:28 -0700

    Initial Commit

---
+++ CalculMath/CalculMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculMathApp.swift
+//  CalculMath
+//
+//  Created by Anatoliy Olyva on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculMathTests/CalculMathTests.swift
@@ -0,0 +1,16 @@
+//
+//  CalculMathTests.swift
+//  CalculMathTests
+//
+//  Created by Anatoliy Olyva on 3/13/25.
+//
+
+import Testing
+
+struct CalculMathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
